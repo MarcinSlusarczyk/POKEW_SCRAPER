4175cbc5a60ce995d0d0bbe683d926fc7f079b22
MarcinSlusarczyk <83789546+MarcinSlusarczyk@users.noreply.github.com>
2021-10-25 20:52:40 +0200

    Update FIND_OFFER_POKEW.py

--- FIND_OFFER_POKEW.py
+++ FIND_OFFER_POKEW.py
@@ -11,31 +11,36 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
-gmail_address = pg.prompt(text='wpisz swoj email')
-password = pg.password('wpisz hasło do swojego konta', mask='*')
-your_sale_price_level = pg.prompt('wpisz wysokość promocji produktu')
+gmail_address = pg.prompt(text='wpisz swoj email na który ma przyjść powiadomienie')
+password = pg.password('wpisz hasło do swojego konta google', mask='*')
+your_sale_price_level = pg.prompt('wpisz wysokość promocji produktu (w PLN)')
+
+if gmail_address == "" and password == "" and your_sale_price_level == "":
+    pg.alert('BRAK INFORMACJI! - URUCHOM PROGRAM OD NOWA I UZUPEŁNIJ ODPOWIEDNIO WSZYSTKIE POLA', 'UZUPEŁNIJ INFORMACJE!')
+    sys.exit()
+
 
 def send_email():
 
-    subject = f'BOT ALERT z PokeWave.eu! - {product_name}'
+    subject = f'BOT ALERT (PokeWave.eu) - {product_name}'
 
     msg = MIMEMultipart()
     msg['From'] = gmail_address
     msg['To'] = gmail_address
     msg['Subject'] = subject
 
-    body = f'link do produktu: {product_link}'
-    msg.attach(MIMEText(body,'plain'))
+    body = f'PROMOCJA o {product_previous_price-product_price} zł !!!! -- link do produktu: {product_link}'
+    msg.attach(MIMEText(body, 'plain'))
 
-    part = MIMEBase('application','octet-stream')
+    part = MIMEBase('application', 'octet-stream')
 
     text = msg.as_string()
-    server = smtplib.SMTP('smtp.gmail.com',587)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(gmail_address, password)
 
 
-    server.sendmail(gmail_address,gmail_address,text)
+    server.sendmail(gmail_address, gmail_address, text)
     server.quit()
 
 
@@ -70,11 +75,13 @@ while True:
                 
                 product_previous_price = int(float(product_price.text.replace('zł', '').replace(',', '.').split()[0]))
                 product_price = int(float(product_price.text.replace('zł', '').replace(',', '.').split()[1]))
+                sale = product_previous_price-product_price
 
-                if product_previous_price-product_price > int(your_sale_price_level):
+                if sale > int(your_sale_price_level):
                     counter += 1
-                    if counter > counter_max:
-                        print(f'SALE LEVEL is: {product_previous_price-product_price} --- {product_name} --- product link: {product_link}')
+                    if counter > counter_max or sale > sale_max:
+                        print(f'Wysyłam powiadomienie: Promocja o: {sale} zł --- {product_name} --- product link: {product_link}')
+                        sale_max = sale
                         send_email()
     time.sleep(1)
     counter_max = counter
