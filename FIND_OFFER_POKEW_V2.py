cb0b665c1e67376e9ca779f5113c2db7f95ac2ca
MarcinSlusarczyk <83789546+MarcinSlusarczyk@users.noreply.github.com>
2021-11-14 15:35:59 +0100

    Add files via upload

---
+++ FIND_OFFER_POKEW_V2.py
@@ -0,0 +1,246 @@
+import time
+import os
+import sys
+import datetime
+import tkinter
+import random
+import requests
+from bs4 import BeautifulSoup
+import pymsgbox as pg
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+
+gmail_address = pg.prompt(text='wpisz swoj email na który ma przyjść powiadomienie')
+password = pg.password('wpisz hasło do swojego konta google', mask='*')
+
+
+if gmail_address == "" and password == "":
+    pg.alert('BRAK INFORMACJI! - URUCHOM PROGRAM OD NOWA I UZUPEŁNIJ ODPOWIEDNIO WSZYSTKIE POLA', 'UZUPEŁNIJ INFORMACJE!')
+    sys.exit()
+
+begin_time = datetime.datetime.now()
+product_name = ""
+counter_loop = 0
+counter_max = 0
+wartosc = 0
+zmiana = 0
+tablica = []
+tablica_start = []
+product_table = []
+link_table = []
+product_table_start = []
+link_table_start = []
+
+def send_email():
+
+    subject = f'BOT ALERT (PokeWave.eu) - {product_name}'
+    
+    msg = MIMEMultipart()
+    msg['From'] = gmail_address
+    msg['To'] = gmail_address
+    msg['Subject'] = subject
+
+    body = f'Cena spadła z {tablica_start[wartosc]} zł na {tablica[wartosc]} !!!! -- link do produktu: {link_table[wartosc]}'
+    msg.attach(MIMEText(body, 'plain'))
+
+    part = MIMEBase('application', 'octet-stream')
+
+    text = msg.as_string()
+
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(gmail_address, password)
+    server.sendmail(gmail_address, gmail_address, text)
+    server.quit()
+
+def send_email_alert():
+    
+    subject = f'PROGRAM 2 - WSZYSTKO DZIAŁA! (BOT ALERT)'
+    
+    msg = MIMEMultipart()
+    msg['From'] = gmail_address
+    msg['To'] = gmail_address
+    msg['Subject'] = subject
+
+    body = f'Wiadomość wygenerowana automatycznie...'
+    msg.attach(MIMEText(body, 'plain'))
+
+    part = MIMEBase('application', 'octet-stream')
+
+    text = msg.as_string()
+
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(gmail_address, password)
+    server.sendmail(gmail_address, gmail_address, text)
+    server.quit()
+    
+
+def send_email_alert_new():
+    
+    subject = f'POJAWIŁ SIĘ NOWY PRODUKT! (BOT ALERT)'
+    
+    msg = MIMEMultipart()
+    msg['From'] = gmail_address
+    msg['To'] = gmail_address
+    msg['Subject'] = subject
+
+    body = f'NOWY PRODUKT! - {product_table[zmiana]} -- link: {product_link[zmiana]}'
+    msg.attach(MIMEText(body, 'plain'))
+
+    part = MIMEBase('application', 'octet-stream')
+
+    text = msg.as_string()
+
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(gmail_address, password)
+    server.sendmail(gmail_address, gmail_address, text)
+    server.quit()
+
+def petla():
+    
+    counter_loop = 0
+    counter_max = 0
+    tablica = []
+    tablica_start = []
+    product_table = []
+    link_table = []
+    product_table_start = []
+    link_table_start = []
+    status_loop = True
+
+    while status_loop:
+        godzina = time.localtime()
+        aktualna = time.strftime("%H:%M:%S", godzina)
+        if aktualna > '08:00:00' and aktualna < '08:00:10':
+            send_email_alert()
+            time.sleep(11)
+        if aktualna > '20:00:00' and aktualna < '20:00:10':            
+            send_email_alert()
+            time.sleep(11)
+        
+        counter_loop +=1
+        counter = 0
+        main_site = 'https://pokewave.eu/produkty.html'    
+        
+        try:
+            page = requests.get(main_site)
+            soup = BeautifulSoup(page.content, "html.parser")
+            index_page = soup.find(class_='IndexStron')
+            page_count = index_page.text[16].replace(' ', '')
+        except:
+            page_count = 6
+        
+        seq = 1
+
+        for seq in range(int(page_count)):
+            
+            try:
+
+                page = (f'{main_site}/s={str(int(seq+1))}')
+                page_request = requests.get(page)
+                soup = BeautifulSoup(page_request.content, "html.parser")
+
+                
+                for index, order in enumerate(soup.find_all(class_='Okno OknoRwd')):
+                    try:
+                        product_name = order.find('h3').text
+                        product_link = order.find('a')['href']
+                        product_status = order.find(class_='ListaOpisowa').text
+                        product_cart = order.find(class_='DoKoszyka')
+                        
+                        try:                        
+                            product_price = order.find('span', class_='CenaPromocyjna').text                            
+                        except:
+                            product_price = 0
+                                            
+                        try:                       
+                            price = order.find(class_='Cena').text
+                        except:
+                            price = 0
+                            
+                        if product_status.find('Dostępny') > 0  and product_cart != None:
+                            
+                            if price == 0:                               
+                                product_price_2 = int(float(product_price.replace(' ', '').replace(',', '.').split('zł')[1]))
+                            else:
+                                product_price_2 =int(float(price.replace(' ', '').replace(',', '.').split('zł')[0]))
+                                
+                            tablica.append(product_price_2)
+                            product_table.append(product_name)
+                            link_table.append(product_link)
+                            
+                            
+                            if counter_loop == 1:
+                                tablica_start.append(product_price_2)                                            
+                                product_table_start.append(product_name)
+                                link_table_start.append(product_link)
+                    except:                        
+                        pass
+                    
+            except requests.exceptions.HTTPError as errh:
+                print ("Http Error:",errh)
+                counter = counter_max
+                time.sleep(5)
+            except requests.exceptions.ConnectionError as errc:
+                print ("Error Connecting:",errc)
+                counter = counter_max
+                time.sleep(5)
+                
+            except requests.exceptions.Timeout as errt:
+                print ("Timeout Error:",errt)
+                counter = counter_max
+                time.sleep(5)
+                
+            except requests.exceptions.RequestException as err:
+                print ("OOps: Something Else",err)
+                counter = counter_max
+                time.sleep(5)
+                       
+        
+        counter_max = counter
+
+        
+        count_tablica = len(tablica)       
+        count_tablica_start = len(tablica_start)
+         
+        if count_tablica > count_tablica_start:
+            for zmiana in range(count_tablica_start):
+                if product_table[zmiana] != product_table_start[zmiana]:
+                    print(product_table[zmiana])
+                    send_email_alert_new()
+                    break
+                    
+        
+        for wartosc in range(count_tablica):
+            try:
+                # print(f'{tablica[wartosc]}, {tablica_start[wartosc]}')
+                if tablica[wartosc] > tablica_start[wartosc]:
+                    counter += 1            
+                    if counter > counter_max:
+                        send_email()
+            except IndexError:
+                print("restart programu")
+                status_loop = False
+                petla()
+                # os.execl(sys.executable, sys.executable, *sys.argv)
+            
+                            
+        counter_max = counter
+        czas = datetime.datetime.now() - begin_time
+        print(f'działa już: {czas} -- wysłano: {counter_max} powiadomień -- ilość produktów: {count_tablica} -- godzina: {aktualna}')
+        time.sleep(1)
+        tablica.clear()
+        product_table.clear()
+        link_table.clear()
+
+petla()
